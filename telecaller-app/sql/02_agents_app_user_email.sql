3a025c25d6923a7107e1c62629ede130d44be73d
amatecdev-mansi <amatec.dev+mansi@gmail.com>
2026-08-22 21:09:08 +0200

    sql: grant telecaller_coaching access to telecaller_app role

--- telecaller-app/sql/02_agents_app_user_email.sql
+++ telecaller-app/sql/02_agents_app_user_email.sql
@@ -15,6 +15,11 @@ UPDATE agents SET app_user_email = 'anirban@amatec.in' WHERE folder_name = 'anir
 UPDATE agents SET app_user_email = 'bhratti@amatec.in' WHERE folder_name = 'bhratti_raval';
 UPDATE agents SET app_user_email = 'paherwarharsha@gmail.com' WHERE folder_name = 'harsha_ahir';
 
+-- telecaller_app is the role the app actually connects as; leads_user is belt and braces.
+GRANT CONNECT ON DATABASE telecaller_coaching TO telecaller_app;
+GRANT USAGE ON SCHEMA public TO telecaller_app;
+GRANT SELECT ON agents, calls, chat_messages TO telecaller_app;
+
 GRANT CONNECT ON DATABASE telecaller_coaching TO leads_user;
 GRANT USAGE ON SCHEMA public TO leads_user;
 GRANT SELECT ON agents, calls, chat_messages TO leads_user;
